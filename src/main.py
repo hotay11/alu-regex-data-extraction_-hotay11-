cb8039ec10e3720f5d58e1d1b5caadf8627d80b1
hotay11 <j.hotay@alustudent.com>
2026-09-05 18:20:53 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -141,13 +141,13 @@ def main():
 
     # Checks the input before extracting any information
     clean_text, suspicious = filter_suspicious_lines(text)
-
+    
     # Extracts and validate email addresses
-    emails = []
+        emails = []
     for match in re.finditer(EMAIL_PATTERN, clean_text, re.IGNORECASE):
         email = match.group()
         if is_well_formed_email(email, match.start(), clean_text):
-            if email not in emails:      
+            if email not in emails:
                 emails.append(email)
     
     # Extracts URLs
